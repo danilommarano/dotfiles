ce5e345058d7114e0ce9799e216877cebed1c888
Danilo Marano <danilo.m.marano@gmail.com>
2022-06-19 20:46:36 -0300

    Configuração do fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,12 @@
+#!/usr/local/bin/fish
+if status is-interactive
+    set fish_greeting
+end
+
+alias vim='nvim'
+
+starship init fish | source
+
+set ght "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"
+
+
