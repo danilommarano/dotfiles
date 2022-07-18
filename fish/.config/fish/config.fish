8eea9be41ab7d62db3a2bafc474d72dd67baf618
Danilo Marano <danilo.m.marano@gmail.com>
2022-07-18 17:34:28 -0300

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,11 +3,5 @@ if status is-interactive
     set fish_greeting
 end
 
-# Some aliases
-alias vim='nvim'
-
 # My prompt shell is starship
 starship init fish | source
-
-# My personal environment variables
-fish ~/.env.fish
