590ce38d9b841392fa5b7d72933552e6ff12609f
Danilo Marano <danilo.m.marano@gmail.com>
2025-08-01 03:46:05 -0300

    :sparkles: Started versioning dotfiles with bash, nvim, tmux and alacritty

---
+++ .config/nvim/init.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader = " " -- isso vem no topo
+
+vim.o.background = "dark" -- or "light"
+
+vim.opt.tabstop = 4        -- número de colunas que um TAB ocupa (visual)
+vim.opt.shiftwidth = 4     -- tamanho da indentação usada por comandos como >> e <<
+vim.opt.expandtab = true   -- converte TABs em espaços reais
+vim.opt.softtabstop = 4    -- ao pressionar <Tab>, insere 4 espaços
+vim.opt.smartindent = true -- autoindent inteligente ao iniciar linhas novas
