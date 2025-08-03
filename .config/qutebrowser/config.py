b481903a492df5aaa39b14425be5be2428dba95e
Danilo Marano <danilo.m.marano@gmail.com>
2025-08-02 23:32:52 -0300

    :sparkles: feat: Saving QutBrowser configuration

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,7 @@
+c.fonts.web.family.standard = "Noto Sans,Noto Color Emoji"
+c.fonts.web.family.sans_serif = "Noto Sans"
+c.fonts.web.family.fixed = "JetBrainsMono Nerd Font"
+c.fonts.web.family.serif = "Noto Serif"
+c.fonts.web.family.cursive = "Noto Sans"
+c.fonts.web.family.fantasy = "Noto Sans"
+
